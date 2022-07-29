187c3c1248368f8c89a85d84bccec363473204d4
Yordan Nikolov <jnikolow93@gmail.com>
2022-07-29 08:50:40 +0100

    navigation bar integrated

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+
+
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+ 
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
+
+  /* </React.StrictMode> */
+
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
