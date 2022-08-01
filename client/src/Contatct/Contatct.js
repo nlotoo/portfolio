a6fc057eb2fe544a8a50f25c7e33e3a2312bea4d
Yordan Nikolov <jnikolow93@gmail.com>
2022-08-01 07:42:23 +0100

    contact button on contact page updated

--- client/src/Contatct/Contatct.js
+++ client/src/Contatct/Contatct.js
@@ -1,4 +1,6 @@
-import { TiMail } from 'react-icons/ti'
+import { TiMail, TiSocialFacebook } from 'react-icons/ti'
+import { AiOutlineWhatsApp } from "react-icons/ai";
+import { Link } from 'react-router-dom'
 import './contatct.css'
 import { ContatctForm } from './cotact-form/ContactForm'
 
@@ -11,14 +13,27 @@ export const Contatct = () => {
 
             <h4 className='heading-contact-page'>Contact Me</h4>
             <div className='contact-page-root'>
-                <div className='contact-me-buttons'>
-                    <button>
-                        <TiMail className='email-icon' /><br/>
-                        Email<br/>
-                        (nlotoo@gmail.com)
-                    </button>
-                    <button>Messanger</button>
-                    <button>WhatsApp</button>
+                <div className='contact-me-a-tag'>
+                    <Link to='#'>
+                        <TiMail className='email-icon' /><br />
+                        Email<br />
+                        (nlotoo93@gmail.com)<br />
+
+                        <strong> Send a message</strong>
+                    </Link>
+                    <Link to='#'>
+                        <TiSocialFacebook className='email-icon' /><br />
+                        Messanger <br />
+                        (nlotoo)<br />
+                        <strong> Send a message</strong>
+
+                    </Link >
+                    <Link to='\web.whatsapp.com/' target="_blank" rel="noopener noreferrer">
+
+                        <AiOutlineWhatsApp className='email-icon' /> <br />
+                        WhatsApp <br />
+                        +447405608112 <br />
+                    </Link>
                 </div>
                 <ContatctForm />
             </div>
