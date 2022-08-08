0a85fb3729d9fa6d04076a76ee15aba92af11467
Yordan Nikolov <jnikolow93@gmail.com>
2022-08-08 22:30:52 +0100

    expirience page updated

--- client/src/components/Experience/Experience.js
+++ client/src/components/Experience/Experience.js
@@ -11,26 +11,42 @@ export const Experience = () => {
             <div className='experience-skill-frame'>
                 <div className='front-end-skills'>
                     <h5>Fontend Development</h5>
-                    <ul>
-                        <li><i className='li-icon-class'><BiBadgeCheck /></i> HTML</li>
-                        <li><i className='li-icon-class'><BiBadgeCheck /></i> CSS</li>
-                        <li><i className='li-icon-class'><BiBadgeCheck /></i> JavaScript</li>
-                        <li><i className='li-icon-class'><BiBadgeCheck /></i> React</li>
+                    <div>
+                        <ul>
+                            <li><i className='li-icon-class'><BiBadgeCheck /></i> HTML</li>
+                            <li><i className='li-icon-class'><BiBadgeCheck /></i> CSS</li>
+                            <li><i className='li-icon-class'><BiBadgeCheck /></i> JavaScript</li>
+                            <li><i className='li-icon-class'><BiBadgeCheck /></i> Angular</li>
 
 
 
-                    </ul>
-                    <ul>
-                        <li><i className='li-icon-class'><BiBadgeCheck /></i> Angular</li>
-                        <li><i className='li-icon-class'><BiBadgeCheck /></i> Bootstrap</li>
-                        <li><i className='li-icon-class'><BiBadgeCheck /></i> MUI</li>
-                        <li><i className='li-icon-class'><BiBadgeCheck /></i> Adobe Photoshop</li>
+                        </ul>
+                        <ul>
+                            <li><i className='li-icon-class'><BiBadgeCheck /></i> React</li>
+                            <li><i className='li-icon-class'><BiBadgeCheck /></i> Bootstrap</li>
+                            <li><i className='li-icon-class'><BiBadgeCheck /></i> MUI</li>
+                            <li><i className='li-icon-class'><BiBadgeCheck /></i> Adobe Photoshop</li>
 
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
                 <div className='back-end-skills'>
                     <h5>Backend Development</h5>
+                    <div>
+                        <ul>
+                            <li><i className='li-icon-class'><BiBadgeCheck /></i> Node.js</li>
+                            <li><i className='li-icon-class'><BiBadgeCheck /></i> Express</li>
 
+
+
+
+                        </ul>
+                        <ul>
+                            <li><i className='li-icon-class'><BiBadgeCheck /></i> MongoDB</li>
+                            <li><i className='li-icon-class'><BiBadgeCheck /></i> Firebase</li>
+
+                        </ul>
+                    </div>
                 </div>
 
             </div>
