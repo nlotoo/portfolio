0b7269efe19f6389c8c65188d5b5be84be54d9e3
Yordan Nikolov <jnikolow93@gmail.com>
2023-01-23 07:21:43 +0000

    responsive desing on certificates

--- client/src/components/Experience/Experience.js
+++ client/src/components/Experience/Experience.js
@@ -10,10 +10,25 @@ import JsBackEnd from '../../public/certificates/JsBackEnd.jpg'
 import JsFundamentals from '../../public/certificates/JsFundamentals.jpg'
 
 import { Link } from 'react-router-dom'
-
+import useWindowDimensions from '../services/windowsDemensions'
+import { useState } from 'react'
 
 
 export const Experience = () => {
+
+    const { height, width } = useWindowDimensions();
+
+    let [showMore, setShowMore] = useState('hideCertificates')
+
+    function showCertificate() {
+
+        if (showMore == 'hideCertificates') {
+            setShowMore('showCertificates')
+        } else {
+            setShowMore('hideCertificates')
+        }
+
+    }
 
     return (
         <section className="experience-root-element" id='experience'>
@@ -93,21 +108,20 @@ export const Experience = () => {
             </div>
             <div className='certificate-container'>
                 <h2 className='cetificate-heading'>Certificates</h2>
-                <ul className='cetificate-carosel'>
-                    <li><div className='certificate-box'><Link target="_blank" to='\softuni.bg/certificates/details/85395/7963ad25'><img className='certificate' src={HonorableMention} alt='serticate-react'></img></Link></div></li>
-                    <li><div className='certificate-box'><Link target="_blank" to='\softuni.bg/certificates/details/96778/97691043'><img className='certificate' src={JsFundamentals} alt='serticate-react'></img></Link></div></li>
-                    <li><div className='certificate-box'><Link target="_blank" to='\softuni.bg/certificates/details/98324/910d7d01'><img className='certificate' src={JsAdvanced} alt='serticate-react'></img></Link></div></li>
-                    <li><div className='certificate-box'><Link target="_blank" to='\softuni.bg/certificates/details/102393/e7ab6299'><img className='certificate' src={JSApplications} alt='serticate-react'></img></Link></div></li>
+                <ul className='cetificate-tray'>
+                    <span className={showMore}>
+                    <li ><div className='certificate-box'><Link target="_blank" to='\softuni.bg/certificates/details/85395/7963ad25'><img className='certificate' src={HonorableMention} alt='serticate-react'></img></Link></div></li>
+                    <li ><div className='certificate-box'><Link target="_blank" to='\softuni.bg/certificates/details/96778/97691043'><img className='certificate' src={JsFundamentals} alt='serticate-react'></img></Link></div></li>
+                    <li ><div className='certificate-box'><Link target="_blank" to='\softuni.bg/certificates/details/98324/910d7d01'><img className='certificate' src={JsAdvanced} alt='serticate-react'></img></Link></div></li>
+                    </span>
+                    <li ><div className='certificate-box'><Link target="_blank" to='\softuni.bg/certificates/details/102393/e7ab6299'><img className='certificate' src={JSApplications} alt='serticate-react'></img></Link></div></li>
                     <li><div className='certificate-box'><Link target="_blank" to='\softuni.bg/certificates/details/108981/533e06d5'><img className='certificate' src={JsBackEnd} alt='serticate-react'></img></Link></div></li>
                     <li><div className='certificate-box'><Link target="_blank" to='\softuni.bg/certificates/details/147278/c4eb4c7b'><img className='certificate' src={HTMLandCSS} alt='serticate-react'></img></Link></div></li>
                     <li><div className='certificate-box'><Link target="_blank" to='\softuni.bg/certificates/details/152915/29f71e9b'><img className='certificate' src={AngularJS} alt='serticate-react'></img></Link></div></li>
                     <li><div className='certificate-box'><Link target="_blank" to='\softuni.bg/certificates/details/140749/2b013d3f'><img className='certificate' src={reactSt} alt='serticate-react'></img></Link></div></li>
-
                 </ul>
+                {width < 425 && <button className='btn-certificate' onClick={showCertificate}>Show {showMore =='showCertificates'? 'LESS':'MORE'}</button>}
             </div>
-
-
-
         </section >
     )
 }
